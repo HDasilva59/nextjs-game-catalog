89bd7f56f39662aae594bec9745f88ad547bb3fb
Da silva <dasilva.hugo59200@gmail.com>
2022-03-23 16:41:36 +0100

    maked the slugs workings and communicating with the database, showing the games depending on the game we chose

--- pages/[platforms]/index.tsx
+++ pages/[platforms]/index.tsx
@@ -1,29 +1,40 @@
+/* eslint-disable react/jsx-key */
 import type { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
+import { ReactChild, ReactFragment, ReactPortal } from "react";
 import { getDatabase } from "../../src/database";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  console.log(context)
   const PlatformName: string = context.query.platforms
   const ParsedPlatformName = PlatformName.replace("-", " ")
-  console.log(ParsedPlatformName)
   const mongodb = await getDatabase();
-  const games = await mongodb.db().collection("games").find({ platform: { name: ParsedPlatformName } }).toArray();
-  const serialized = JSON.stringify(games);
+  const platforms = await mongodb.db().collection("games").find({ "platform.name" : ParsedPlatformName}).toArray();
+  const serialized = JSON.stringify(platforms);
 
   return {
     props: {
-      games: serialized,
+      platforms: serialized,
+      platformNames : PlatformName
     },
   };
 };
 
 const Game: NextPage = (props) => {
-  const test = JSON.parse(props.games);
-  console.log(test);
+  const gamesPerPlatform = JSON.parse(props.platforms);
+  const gamesNamesPerPlatformArray = gamesPerPlatform.map((element) => [element.name, element.cover.url, element.slug])
+
+  function displayGames (games: []) {
+    const allThoseGames = games.map((element) => <div><Link href={`${props.platformNames}/${element[2]}`}><a><p>{element[0]}</p> <img src={element[1]}/></a></Link></div>)
+    return (
+    <div>
+      {allThoseGames}
+    </div>
+    )
+  }
   return (
     <div>
-      <p>{test[0].name}</p>
-      <img src={test[0].cover.url} />
+      {displayGames(gamesNamesPerPlatformArray)}
+
     </div>
   );
 };
